627482e5e6e2e076e34616d528789b2c2aed009a
leumasic <53974949+leumasic@users.noreply.github.com>
2022-09-29 15:58:18 -0400

    chore: remote old eslint file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,21 +9,5 @@ module.exports = {
   root: true,
   rules: {
     "@typescript-eslint/no-unused-vars": "error",
-
-    // '@typescript-eslint/ban-ts-comment': 'off',
-    // '@typescript-eslint/camelcase': 'off',
-    // '@typescript-eslint/explicit-module-boundary-types': 'off',
-    // '@typescript-eslint/no-empty-function': 'off',
-    // '@typescript-eslint/no-explicit-any': 'off',
-    // '@typescript-eslint/no-non-null-assertion': 'off',
-    // '@typescript-eslint/no-unused-vars': 'off',
-    // '@typescript-eslint/no-use-before-define': 'off',
-    // '@typescript-eslint/no-var-requires': 'off',
-    // '@typescript-eslint/no-this-alias': 'off',
-    // 'no-console': 'warn',
-    // 'prefer-const': 'off',
-    // 'no-shadow': 'off',
-    // '@typescript-eslint/no-shadow': ['error'],
-    // 'no-only-tests/no-only-tests': 'error',
   },
 };
