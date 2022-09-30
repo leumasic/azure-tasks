24e51b77bd4570ef3de8f242936affdddf10ca1e
leumasic <53974949+leumasic@users.noreply.github.com>
2022-09-30 10:10:48 -0400

    chore: copy pasta inputs and outputs from changesets repo

--- packages/azure-changeset-task/src/index.ts
+++ packages/azure-changeset-task/src/index.ts
@@ -3,14 +3,16 @@ import tl from 'azure-pipelines-task-lib/task';
 async function run() {
     try {
         const inputString: string | undefined = tl.getInput('samplestring', true);
-        if (inputString == 'bad') {
+
+        if (inputString === 'bad') {
             tl.setResult(tl.TaskResult.Failed, 'Bad input was given');
             return;
         }
+
         console.log('Hello', inputString);
     }
     catch (err) {
-        tl.setResult(tl.TaskResult.Failed, err.message);
+        tl.setResult(tl.TaskResult.Failed, (err as any).message);
     }
 }
 
